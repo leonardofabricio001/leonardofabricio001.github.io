d211b0aa1dd1429007e11ab08b1337882a550323
Leofz <>
2022-11-23 10:59:12 -0400

    added python info

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let outputTexts = new Object();
 outputTexts = {
       "html": "HTML (Hypertext Markup Language ou Linguagem de Marcação de HiperTexto) é o bloco de construção mais básico da web. Define o significado e a estrutura do conteúdo da web.",
       "css": "CSS (Cascading Style Sheets ou Folhas de Estilo em Cascata) é uma linguagem de estilo usada para descrever a apresentação de um documento escrito em HTML ou em XML. O CSS descreve como elementos são mostrados na tela, no papel, na fala ou em outras mídias.",
-      "js": "O JavaScript é uma linguagem interpretada baseada em protótipos, multi-paradigma e dinâmica, suportando estilos de orientação a objetos, imperativos e declarativos (como por exemplo a programação funcional).",
+      "js": "O JavaScript é uma linguagem de programação interpretada baseada em protótipos, multi-paradigma e dinâmica, suportando estilos de orientação a objetos, imperativos e declarativos (como por exemplo a programação funcional).",
+      "py": "Python é uma linguagem de programação interpretada, orientada a objetos e de alto nível.",
       "git": "O Git é um software de controle de versão que proporciona versionamento de código."
 
 }
@@ -32,6 +33,10 @@ cssHover.addEventListener("mouseout", defaultSettings);
 let jsHover = document.getElementById("js");
 jsHover.addEventListener("mouseover", setOutputSettings);
 jsHover.addEventListener("mouseout", defaultSettings);
+
+let pyHover = document.getElementById("py");
+pyHover.addEventListener("mouseover", setOutputSettings);
+pyHover.addEventListener("mouseout", defaultSettings);
 
 let gitHover = document.getElementById("git");
 gitHover.addEventListener("mouseover", setOutputSettings);
